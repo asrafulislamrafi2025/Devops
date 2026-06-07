1ba3c6e0069a6cef47296124edc45adadb99cda9
Rafi <asrafulrafi2024@gmail.com>
2026-06-08 00:34:01 +0100

    new relased code for python

--- app.py
+++ app.py
@@ -8,3 +8,16 @@ while i<=6:
 print(sum)
       i=i+1
 
+username="enter the username:"
+password="enter the password:"
+
+if username=="rafi":
+     print("login success")
+else:
+     print("try again")
+
+if password=="r@fi120"
+     print("welcome to python")
+else:
+     print("wrong password")
+
